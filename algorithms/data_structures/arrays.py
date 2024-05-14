58ceffca43694ea95a2c54bb0aa8e982cf09664b
xom9s <xom9k321@gmail.com>
2024-05-14 22:22:20 +0300

    array practice

---
+++ algorithms/data_structures/arrays.py
@@ -0,0 +1,131 @@
+# The difference between an array and a list is that the type of object stored in the array container is constrained.
+
+import array as arr
+
+""" Creating an array """
+import array as arr
+a = arr.array('i', [1, 2, 3])
+print("The new created array is : ", end=" ")
+for i in range(0, 3):
+    print(a[i], end=" ")
+print()
+b = arr.array('d', [2.5, 3.2, 3.3])
+print("\nThe new created array is : ", end=" ")
+for i in range(0, 3):
+    print(b[i], end=" ")
+
+""" Adding Elements to a Array """
+
+a = arr.array('i', [1, 2, 3])
+print("Array before insertion : ", end=" ")
+for i in range(0, 3):
+    print(a[i], end=" ")
+print()
+a.insert(1, 4)
+print("Array after insertion : ", end=" ")
+for i in (a):
+    print(i, end=" ")
+print()
+b = arr.array('d', [2.5, 3.2, 3.3])
+print("Array before insertion : ", end=" ")
+for i in range(0, 3):
+    print(b[i], end=" ")
+print()
+b.append(4.4)
+print("Array after insertion : ", end=" ")
+for i in (b):
+    print(i, end=" ")
+print()
+
+""" Removing Elements from the Array """
+
+import array
+arr = array.array('i', [1, 2, 3, 1, 5])
+print("The new created array is : ", end="")
+for i in range(0, 5):
+    print(arr[i], end=" ")
+
+print("\r")
+print("The popped element is : ", end="")
+print(arr.pop(2))
+print("The array after popping is : ", end="")
+for i in range(0, 4):
+    print(arr[i], end=" ")
+
+print("\r")
+arr.remove(1)
+print("The array after removing is : ", end="")
+for i in range(0, 3):
+    print(arr[i], end=" ")
+
+"""Slicing of an Array"""
+
+import array as arr
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+a = arr.array('i', l)
+print("Initial Array: ")
+for i in (a):
+    print(i, end=" ")
+Sliced_array = a[3:8]
+print("\nSlicing elements in a range 3-8: ")
+print(Sliced_array)
+Sliced_array = a[5:]
+print("\nElements sliced from 5th "
+      "element till the end: ")
+print(Sliced_array)
+Sliced_array = a[:]
+print("\nPrinting all elements using slice operation: ")
+print(Sliced_array)
+
+""" Searching Element in an Array """
+
+import array
+arr = array.array('i', [1, 2, 3, 1, 2, 5])
+print("The new created array is : ", end="")
+for i in range(0, 6):
+    print(arr[i], end=" ")
+
+print("\r")
+print("The index of 1st occurrence of 2 is : ", end="")
+print(arr.index(2))
+print("The index of 1st occurrence of 1 is : ", end="")
+print(arr.index(1))
+
+""" Updating Elements in a Array """
+
+import array
+arr = array.array('i', [1, 2, 3, 1, 2, 5])
+print("Array before updation : ", end="")
+for i in range(0, 6):
+    print(arr[i], end=" ")
+
+print("\r")
+arr[2] = 6
+print("Array after updation : ", end="")
+for i in range(0, 6):
+    print(arr[i], end=" ")
+print()
+arr[4] = 8
+print("Array after updation : ", end="")
+for i in range(0, 6):
+    print(arr[i], end=" ")
+
+""" Extend Element from Array """
+
+import array as arr 
+a = arr.array('i', [1, 2, 3,4,5])
+print("The before array extend  : ", end =" ")
+for i in range (0, 5): 
+  
+    print (a[i], end =" ") 
+    
+print()
+a.extend([6,7,8,9,10])
+print("\nThe array after extend :",end=" ")
+
+for i in range(0,10):  
+  
+    print(a[i],end=" ") 
+    
+print()
