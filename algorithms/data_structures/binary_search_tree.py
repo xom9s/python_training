c74364056f2ab4a70ee2d2f734e176edb6040be5
xom9s <xom9k321@gmail.com>
2024-05-17 22:58:09 +0300

    binary search tree practice

---
+++ algorithms/data_structures/binary_search_tree.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        if self.root is None:
+            self.root = Node(key)
+        else:
+            self._insert_recursive(self.root, key)
+
+    def _insert_recursive(self, node, key):
+        if key < node.val:
+            if node.left is None:
+                node.left = Node(key)
+            else:
+                self._insert_recursive(node.left, key)
+        else:
+            if node.right is None:
+                node.right = Node(key)
+            else:
+                self._insert_recursive(node.right, key)
+
+    def search(self, key):
+        return self._search_recursive(self.root, key)
+
+    def _search_recursive(self, node, key):
+        if node is None or node.val == key:
+            return node
+        if key < node.val:
+            return self._search_recursive(node.left, key)
+        return self._search_recursive(node.right, key)
+
+    def inorder_traversal(self):
+        elements = []
+        self._inorder_recursive(self.root, elements)
+        return elements
+
+    def _inorder_recursive(self, node, elements):
+        if node:
+            self._inorder_recursive(node.left, elements)
+            elements.append(node.val)
+            self._inorder_recursive(node.right, elements)
